c09297658d3f082d8636eca3757a49e13d00fd1c
Pranjali Aditi <pranjaliaditi2003@gmail.com>
2024-02-18 18:26:32 +0530

    basic binary search

--- binary_search/binary_basic.cpp
+++ binary_search/binary_basic.cpp
@@ -1,7 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool binary(int arr[], int num, int n){
+    
+    sort(arr,arr+n);
+
+    int l = 0;
+    int r = n-1;
+    
+    while(l<=r){
+        int mid = (l+r)/2;
+
+        if(num < arr[mid]){
+            r = mid - 1;
+        }
+        
+
+        else if(num > arr[mid]){
+            l = mid+1;
+        }
+
+        else{
+            return true;
+        }
+    }
+
+    return false;
+}
+
 int main(){
-    int t = 10;
-    cout << t;
-    return 0;
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i =0; i<n; i++)
+        cin >> arr[i];
+    int num;
+    cin >> num;
+
+    if( binary(arr,num,n))
+    cout << "Present";
+
+    else
+    cout << "Not Present";
 }
