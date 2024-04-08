9c4d2536b6386f7074194d1895f7a2dc894936d7
Pranjali Aditi <pranjaliaditi2003@gmail.com>
2024-04-08 21:32:05 +0530

    tower of hanoi.cpp solved

---
+++ CSES/Introductory problems/Tower_of_Hanoi.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<char, char>> seq;
+
+void hanoi(int n, char from, char to, char use) {
+    if (n == 0) {
+        return;
+    }
+
+    hanoi(n - 1, from, use, to); 
+    seq.push_back(make_pair(from, to)); 
+    hanoi(n - 1, use, to, from); 
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    hanoi(n, '1', '3', '2'); 
+
+    cout << seq.size() << "\n";
+
+    for (int i = 0; i < seq.size(); i++) {
+        cout << seq[i].first << " " << seq[i].second << "\n";
+    }
+}
