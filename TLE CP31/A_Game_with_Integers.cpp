6e6d093b7e12615be21c7f76326a877828df1a65
Pranjali Aditi <pranjaliaditi2003@gmail.com>
2024-07-22 16:54:50 +0530

    Create A_Game_with_Integers.cpp

---
+++ TLE CP31/A_Game_with_Integers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        if(n%3) cout << "First \n";
+        else cout << "Second \n";
+
+    }
+}
